05858b8e3b2c5b408288e7cbd5ded02e025ffe42
Navruzbek2103 <navrozbek2306@gmail.com>
2022-07-02 10:50:25 +0500

    Create first js work

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var user = "Foydalanuvchining ";
+var savol1 = prompt("Ismingizni kiriting:");
+var savol2 = prompt("Familiyangizni kiriting:");
+var savol3 = prompt("Yoshingizni kiriting:");
+var savol4 = prompt("Kasbingizni kiriting: \n 👨🏻‍🎓 \t 👷‍♂️ \t 👨‍🍳");
+var savol5 = prompt("Manzilingizni kiriting: \n 🗺");
+var savol6 = prompt("Siz oilangizda nechanchi farzandsiz? \n 👨‍👨‍👧");
+var savol7 = prompt("Oilangiz necha nafardan iborat? \n 👩‍👩‍👧‍👦");
+var savol8 = prompt("Yoqtirgan rangingiz qaysi? \n 🙄");
+var savol9  = prompt("Yoqtirgan faningiz qaysi? \n 😵‍💫");
+var savol10 = prompt("Hozirda qayerda yashaysiz? \n ⛺️");
+var savol11 = prompt("Ayni damda biror ish bilan shug'ullanayapsizmi? \n \n🔨 \n \n Ha  \t Yo'q, endi");
+var savol12 = prompt("Shaxsiy biznesingiz bormi? \n 💰");
+var savol13 = prompt("Fudbol sizga yoqadimi? \n ⚽️:");
+var savol14 = prompt("Hozirda qancha mablag' jamg'arib qo'ygansiz? (Aytavering, uyalmang...) \n 💵");
+var savol15 = prompt("Elektronikaga qiziqishingiz bormi? \n 👨‍🏭");
+var savol16 = prompt("Amalga oshgan niyatingiz bormi? \n bor \t \t yo'q \n 🤔");
+var savol17 = prompt("Hisob-kitob ishlari sizga osonmi? \n oson \t \t qiyin \n 🧮");
+var savol18 = prompt("Shaxmat o'ynashni bilasizmi? \n ♟");
+var savol19 = prompt("Qaysi meva siz uchun sevimli? \n 🍐");
+var savol20 = prompt("To'ldirgan ma'lumotlaringiz rost ekanligini tasdiqlaysizmi? \n ha \t \t yo'q \t ✅");
+
+
+alert(user + "ismi: \t \t \t \t" + savol1 + "\n" + savol1 + "ning familiyasi: \t \t \t \t" + savol2 + '\n' + savol1 + "ning yoshi: \t \t \t \t" + savol3 + '\n' + savol1 + "ning kasbi: \t \t \t \t" + savol4 + '\n' + savol1 + "ning manzili: \t \t \t \t" + savol5 + '\n' + savol1 + " oilasida " + savol6 + "-farzand \n" + savol1 + 'ning oilasi ' + savol7 + ' kishidan iborat \n' + savol1 + "ning sevimli rangi: \t \t \t \t" + savol8 + '\n' + savol1 + 'ning yoqtirgan fani: \t \t \t \t' + savol9 + '\n' + savol1 + " hozirda " + savol10 + "da yashayapti \n" + savol1 + " ayni damda bandmikan?: \t \t \t \t" + savol11 + '\n' + savol1 + "ning shaxsiy biznesi \t \t \t \t" + savol12 + '\n' + savol1 + "ning fudbolga qiziqishi \t \t \t \t" + savol13 + '\n' + savol1 + "ning jamg'arib qo'ygan puli \t \t \t \t" + savol14 + '\n' + savol1 + "ning elektronikaga qiziqishi \t \t \t \t" + savol15 + '\n' + savol1 + "ning amalga oshgan niyati \t \t \t \t" + savol16 + '\n' + savol1 + "ga hisob-kitob ishlari: \t \t \t \t" + savol17 + '\n' + savol1 + " shaxmatni: \t \t \t \t" + savol18 + '\n' + savol1 + "ning sevimli mevasi: \t \t \t \t" + savol19 + '\n' + savol1 + " kiritgan ma'lumotlar to'g'rimikan: \t \t \t \t" + savol20 + '\n');
+
+console.log(user + "ismi: \t \t \t \t" + savol1 + "\n" + savol1 + "ning familiyasi: \t \t \t \t" + savol2 + '\n' + savol1 + "ning yoshi: \t \t \t \t" + savol3 + '\n' + savol1 + "ning kasbi: \t \t \t \t" + savol4 + '\n' + savol1 + "ning manzili: \t \t \t \t" + savol5 + '\n' + savol1 + " oilasida " + savol6 + "-farzand \n" + savol1 + 'ning oilasi ' + savol7 + ' kishidan iborat \n' + savol1 + "ning sevimli rangi: \t \t \t \t" + savol8 + '\n' + savol1 + 'ning yoqtirgan fani: \t \t \t \t' + savol9 + '\n' + savol1 + " hozirda " + savol10 + "da yashayapti \n" + savol1 + " ayni damda bandmikan?: \t \t \t \t" + savol11 + '\n' + savol1 + "ning shaxsiy biznesi \t \t \t \t" + savol12 + '\n' + savol1 + "ning fudbolga qiziqishi \t \t \t \t" + savol13 + '\n' + savol1 + "ning jamg'arib qo'ygan puli \t \t \t \t" + savol14 + '\n' + savol1 + "ning elektronikaga qiziqishi \t \t \t \t" + savol15 + '\n' + savol1 + "ning amalga oshgan niyati \t \t \t \t" + savol16 + '\n' + savol1 + "ga hisob-kitob ishlari: \t \t \t \t" + savol17 + '\n' + savol1 + " shaxmatni: \t \t \t \t" + savol18 + '\n' + savol1 + "ning sevimli mevasi: \t \t \t \t" + savol19 + '\n' + savol1 + " kiritgan ma'lumotlar to'g'rimikan: \t \t \t \t" + savol20 + '\n');
